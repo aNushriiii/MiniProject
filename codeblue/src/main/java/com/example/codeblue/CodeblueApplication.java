43ddd3cfc5eb0674e7ed5893f4d33489d091be90
aNushriiii <147025481+aNushriiii@users.noreply.github.com>
2024-01-28 13:48:52 +0530

    created project
    added initial files

---
+++ codeblue/src/main/java/com/example/codeblue/CodeblueApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeblueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeblueApplication.class, args);
+	}
+
+}
